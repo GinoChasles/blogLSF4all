eccfabc0e6d932849717838d8785b90f0248bb79
GinoChasles <gino.chasles@gmail.com>
2021-03-15 14:44:34 +0100

    strapi && gatsby

---
+++ blog-strapi/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a3410ee0fc4c1d081cefe4952b1b72a1'),
+    },
+  },
+});
---
+++ blog-strapi/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3add8f3e-4d14-4ba3-b6a7-3edc96fe3c89'
+};
